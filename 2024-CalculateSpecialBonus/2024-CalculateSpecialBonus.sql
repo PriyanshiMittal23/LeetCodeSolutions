4c8668da40d18739b7946e163440df05140424ab
Priyanshi Mittal <106468181+PriyanshiMittal23@users.noreply.github.com>
2025-08-06 23:07:57 +0530

    Create 2024-CalculateSpecialBonus

---
+++ 2024-CalculateSpecialBonus/2024-CalculateSpecialBonus.sql
@@ -0,0 +1,6 @@
+-- Last updated: 6/8/2025, 11:07:57 pm
+# Write your MySQL query statement below
+select employee_id,
+    if(employee_id%2!=0 and name NOT LIKE 'M%',salary,0) as bonus 
+    from Employees 
+    order by employee_id;
