7d2a8ae1440d66a06c72d374fb6bd4a2a138eaf0
Priyanshi Mittal <106468181+PriyanshiMittal23@users.noreply.github.com>
2025-08-06 23:11:06 +0530

    Create 507-PerfectNumber

---
+++ 507-PerfectNumber/507-PerfectNumber.java
@@ -0,0 +1,8 @@
+// Last updated: 6/8/2025, 11:11:06 pm
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        return num == 6 || num == 28 ||
+        num == 496 || num == 8128 ||
+        num == 33550336;
+    }
+}
