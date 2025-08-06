b6bfaaf96896b1eedce875c07d705eae416ad55a
Priyanshi Mittal <106468181+PriyanshiMittal23@users.noreply.github.com>
2025-08-06 23:09:02 +0530

    Create 1378-CellsWithOddValuesInAMatrix

---
+++ 1378-CellsWithOddValuesInAMatrix/1378-CellsWithOddValuesInAMatrix.java
@@ -0,0 +1,20 @@
+// Last updated: 6/8/2025, 11:09:01 pm
+class Solution {
+    public int oddCells(int m, int n, int[][] indices) {
+        int []r=new int[m];
+        int []c=new int[n];
+        for(int a[]:indices){
+            r[a[0]]++;
+            c[a[1]]++;
+        }
+        int ans=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if((r[i]+c[j])%2==1){
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+}
