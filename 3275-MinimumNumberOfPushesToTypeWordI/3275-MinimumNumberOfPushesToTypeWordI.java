c52c6b21cbbbcf2662cec1f521d5411f4ce3aaaa
Priyanshi Mittal <106468181+PriyanshiMittal23@users.noreply.github.com>
2025-08-06 23:05:28 +0530

    Create 3275-MinimumNumberOfPushesToTypeWordI

---
+++ 3275-MinimumNumberOfPushesToTypeWordI/3275-MinimumNumberOfPushesToTypeWordI.java
@@ -0,0 +1,16 @@
+// Last updated: 6/8/2025, 11:05:28 pm
+class Solution {
+    public int minimumPushes(String word) {
+        HashMap<Character,Integer> hm = new HashMap<>();
+        for(char ch: word.toCharArray()){
+            hm.put(ch,hm.getOrDefault(ch,0)+1);
+        }
+        List<Integer> l = new ArrayList<>(hm.values());
+        Collections.sort(l,Collections.reverseOrder());
+        int press = 0;
+        for(int i=0;i<l.size();i++){
+            press += (i/8+1)*l.get(i);
+        }
+        return press;
+    }
+}
