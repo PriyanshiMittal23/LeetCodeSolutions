61df8df0d4d5ad0f7dbdecf38f5f2d36b2d67af1
Priyanshi Mittal <106468181+PriyanshiMittal23@users.noreply.github.com>
2025-08-06 23:04:52 +0530

    Create 3495-KThNearestObstacleQueries

---
+++ 3495-KThNearestObstacleQueries/3495-KThNearestObstacleQueries.java
@@ -0,0 +1,19 @@
+// Last updated: 6/8/2025, 11:04:52 pm
+class Solution {
+    public int[] resultsArray(int[][] queries, int k) {
+        int n = queries.length;
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        int[] res = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            int sum = Math.abs(queries[i][0]) + Math.abs(queries[i][1]);
+            pq.add(sum);
+            if (pq.size() > k) {
+                pq.poll();
+            }
+            res[i] = (pq.size() == k) ? pq.peek() : -1;
+        }
+        
+        return res;
+    }
+}
