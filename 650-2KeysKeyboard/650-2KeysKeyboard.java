203cb35be6494b303306e11c47dd3af7fb80be0a
Priyanshi Mittal <106468181+PriyanshiMittal23@users.noreply.github.com>
2025-08-06 23:10:35 +0530

    Create 650-2KeysKeyboard

---
+++ 650-2KeysKeyboard/650-2KeysKeyboard.java
@@ -0,0 +1,17 @@
+// Last updated: 6/8/2025, 11:10:34 pm
+class Solution {
+    
+    public int minSteps(int n) {
+        int ans=0;
+        for(int i=2;i*i<=n;i++){
+            while(n%i==0){
+                ans+=i;
+                n=n/i;
+            }
+        }
+        if(n!=1){
+            ans+=n;
+        }
+        return ans;
+    }
+}
