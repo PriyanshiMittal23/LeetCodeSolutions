28226ed7542a4ff09bbfbcaa49036219d7dff4fc
Priyanshi Mittal <106468181+PriyanshiMittal23@users.noreply.github.com>
2025-08-06 23:13:27 +0530

    Create 135-Candy

---
+++ 135-Candy/135-Candy.java
@@ -0,0 +1,23 @@
+// Last updated: 6/8/2025, 11:13:27 pm
+class Solution {
+    public int candy(int[] ratings) {
+        int n = ratings.length;
+        int[]c=new int[n];
+        Arrays.fill(c,1);
+        for(int i=1;i<n;i++){
+            if(ratings[i]>ratings[i-1]){
+                c[i]=c[i-1]+1;
+            }
+        }
+        for(int i=n-2;i>=0;i--){
+            if(ratings[i]>ratings[i+1] && c[i]<=c[i+1]){
+                c[i]=c[i+1]+1;
+            }
+        }
+        int sum=0;
+        for(int a:c){
+            sum+=a;
+        }
+        return sum;
+    }
+}
