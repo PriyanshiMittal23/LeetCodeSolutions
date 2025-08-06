e48ad182d23e72b6e2e98a58dbbb535f859be392
Priyanshi Mittal <106468181+PriyanshiMittal23@users.noreply.github.com>
2025-08-06 23:08:41 +0530

    Create 1615-RangeSumOfSortedSubarraySums

---
+++ 1615-RangeSumOfSortedSubarraySums/1615-RangeSumOfSortedSubarraySums.java
@@ -0,0 +1,20 @@
+// Last updated: 6/8/2025, 11:08:41 pm
+class Solution {
+    public int rangeSum(int[] nums, int n, int left, int right) {
+        int[]arr=new int[n*(n+1)/2];
+        int a=0;
+        for(int i=0;i<n;i++){
+            int sum=0;
+            for(int j=i;j<n;j++){
+                sum = (sum+nums[j])%1000000007;
+                arr[a++]=sum;
+            }
+        }
+        Arrays.sort(arr);
+        int ans=0;
+        for(int i=left-1;i<right;i++){
+            ans=(ans+arr[i])%1000000007;
+        }
+        return ans;
+    }
+}
