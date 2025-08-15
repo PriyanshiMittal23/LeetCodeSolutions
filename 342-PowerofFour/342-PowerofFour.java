382c0b014160862a0aeaf5b4bbdeb178235073af
Priyanshi Mittal <106468181+PriyanshiMittal23@users.noreply.github.com>
2025-08-15 16:44:38 +0530

    File updated at 15/8/2025, 4:44:38 pm

--- 342-PowerofFour/342-PowerofFour.java
+++ 342-PowerofFour/342-PowerofFour.java
@@ -1,8 +1,6 @@
-// Last updated: 15/8/2025, 4:44:02 pm
+// Last updated: 15/8/2025, 4:44:38 pm
 class Solution {
     public boolean isPowerOfFour(int n) {
-        int c=0;
-        int t=n;
         int p = 0;
         if(n<=0){
             return false;
@@ -10,8 +8,8 @@ class Solution {
         if((n&(n-1))!=0){
             return false;
         }
-        while(t>0){
-            t= (t>>1);
+        while(n>0){
+            n= (n>>1);
             p++;
         }
         if(p%2==0){
