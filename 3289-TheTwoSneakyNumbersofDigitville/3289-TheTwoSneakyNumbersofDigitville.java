2540e6dadc0c775ee6b37be28e7012760be9928d
Priyanshi Mittal <106468181+PriyanshiMittal23@users.noreply.github.com>
2025-10-31 22:18:25 +0530

    File updated at 31/10/2025, 10:18:25 pm

--- 3289-TheTwoSneakyNumbersofDigitville/3289-TheTwoSneakyNumbersofDigitville.java
+++ 3289-TheTwoSneakyNumbersofDigitville/3289-TheTwoSneakyNumbersofDigitville.java
@@ -1,20 +1,16 @@
-// Last updated: 31/10/2025, 10:06:53 pm
+// Last updated: 31/10/2025, 10:18:25 pm
 class Solution {
     public int[] getSneakyNumbers(int[] nums) {
+        int[]ans = new int[2];
+        int idx=0;
         HashSet<Integer> hs = new HashSet<>();
-        HashSet<Integer> ans = new HashSet<>();
         for(int i:nums){
             if(hs.contains(i)){
-                ans.add(i);
+                ans[idx++]=i;
             }else{
                 hs.add(i);
             }
         }
-        int[] arr = new int[ans.size()];
-        int idx=0;
-        for(int i:ans){
-            arr[idx++]=i;
-        }
-        return arr;
+        return ans;
     }
 }
