c91253a45e9264e26f43f4509693ea8d6247c565
Priyanshi Mittal <106468181+PriyanshiMittal23@users.noreply.github.com>
2026-01-31 19:57:37 +0530

    File updated at 31/1/2026, 7:57:35 pm

--- 2148-CountElementsWithStrictlySmallerandGreaterElements/2148-CountElementsWithStrictlySmallerandGreaterElements.java
+++ 2148-CountElementsWithStrictlySmallerandGreaterElements/2148-CountElementsWithStrictlySmallerandGreaterElements.java
@@ -1,16 +1,20 @@
-// Last updated: 31/1/2026, 7:54:00 pm
-1class Solution {
-2    public int countElements(int[] nums) {
-3        Arrays.sort(nums);
-4        int s = nums[0];
-5        int l = nums[nums.length-1];
-6        int ans = 0;
-7        for(int a:nums){
-8            if(a<l && a>s){
-9                ans++;
-10            }
-11        }
-12
-13        return ans;
-14    }
-15}
+// Last updated: 31/1/2026, 7:57:35 pm
+class Solution {
+    public int countElements(int[] nums) {
+        int min=Integer.MAX_VALUE;
+        int max=Integer.MIN_VALUE;
+        int cnt=0;
+        for(int i:nums){
+            if(i>max)max=i;
+        }
+        for(int i:nums){
+            if(i<min)min=i;
+        }
+        for(int j:nums){
+            if(j>min && j<max){
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+}
